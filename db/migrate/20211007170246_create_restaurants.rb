561544cf73b509867442e6e5c2b3b113a3c51100
Natalie Bechtold <natalie.bechtold@gmail.com>
2021-10-09 11:13:52 +0200

    done

--- db/migrate/20211007170246_create_restaurants.rb
+++ db/migrate/20211007170246_create_restaurants.rb
@@ -3,9 +3,8 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
     create_table :restaurants do |t|
       t.string :name
       t.string :address
-      t.integer :phone_number
+      t.string :phone_number
       t.string :category
-      t.integer :review_id
 
       t.timestamps
     end
